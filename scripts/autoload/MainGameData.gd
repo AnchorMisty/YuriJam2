d34db55d0e2b0f17bf913d0c1b00e539f1b776dc
Ancy <2592409776@qq.com>
2026-06-02 13:50:23 +0800

    修改了一下文件目录

---
+++ scripts/autoload/MainGameData.gd
@@ -0,0 +1,26 @@
+extends Node
+
+'''
+在这里存放游戏的数据（都是全局！！
+打算吧ready和process放在最下面↓↓↓
+'''
+
+### ————————————————————————————————数据区域————————————————————————————————————
+
+var day = 1
+
+var chara_names = {
+	"Lin": "Lin",
+	"Yao": "Yao"
+}
+
+### ———————————————————————————————————————————————————————————————————————————
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
